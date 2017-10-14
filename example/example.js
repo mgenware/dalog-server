59c8f8e1f08c904f73518abd1d983b50d7f61964
Mgen <m@mgenware.com>
2017-10-14 18:41:26 +0800

    Update example.js

--- example/example.js
+++ example/example.js
@@ -1,13 +1,27 @@
 const bb = require('..');
 
-const colorLogger = new bb.Logger(new bb.ConsoleProvider({ showColor: true }));
-colorLogger.info('user.http.get', {
-  request: '/user/1',
-  platform: 'ios',
-});
-colorLogger.error('user.http.update', {
-  status: 404,
-  id: 123,
-});
+function funWithLogger(name, logger) {
+  console.log(`======== ${name} ========`);
+  logger.verbose('user.profile.click', {
+    target: 123,
+    src: 32,
+  });
+  logger.info('user.http.get', {
+    request: '/user/1',
+    platform: 'ios',
+  });
+  logger.warning('memory.warning', {
+    used: '400mb',
+  });
+  logger.error('user.http.update', {
+    status: 404,
+    id: 123,
+  });
+  console.log();
+}
 
 const plainLogger = new bb.Logger(new bb.ConsoleProvider());
+const colorLogger = new bb.Logger(new bb.ConsoleProvider({ showColor: true }));
+
+funWithLogger('Plain console logger', plainLogger);
+funWithLogger('Color console logger', colorLogger);
